28e73743ed0f9f1bca74ffc3e7cab09cf9ff77ae
Anson <1072476988@qq.com>
2020-11-10 12:38:31 +0800

    [Lingzhen] feat: finish excrcise1.

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,14 @@
-const parseData
+const parseData = ({data, column}) => {
+    const returnArray = [];
+    for (let i = 0; i < data.length; i++) {
+        const people = data[i];
+        const returnObj = {};
+        for (let j = 0; j < column.length; j++) {
+            returnObj[column[j].name] = people[j];
+        }
+        returnArray.push(returnObj);
+    }
+    return returnArray;
+}
+
 export { parseData };
