888bcdbddc04177f2f24d782686ca07116f03cd4
Anson <1072476988@qq.com>
2020-11-10 13:29:29 +0800

    [Lingzhen] feat: finish practice2.

--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,8 @@
-const inject
+const inject = (items, sections) => {
+    for (let i = 0; i < sections.length; i++) {
+        const {content, index} = sections[i];
+        items.splice(index,1,[content,items[index]]);
+    }
+    return items.flat();
+}
 export { inject };
